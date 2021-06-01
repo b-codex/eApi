1c9b5f7aeb25f299cd4df7a75f5fba60ba50efef
Bisrat Fekede <61845339+b-codex@users.noreply.github.com>
2021-06-02 00:52:57 +0300

    configured blueprint

--- app.py
+++ app.py
@@ -1,37 +1,6 @@
-from flask import Flask
 from routes.routes import *
 
-app = Flask(__name__)
-app.register_blueprint(second, url_prefix="/ad")
-
-from flask import Flask
-from flask_restplus import Api, Resource, fields
-
-app = Flask(__name__)
-api = Api(app)
-
-a_lang = api.model('Language', {'language' : fields.String('The language.')})
-
-langs = []
-python = {'language' : 'python', 'id' : 1}
-langs.append(python)
-
-@api.route('/lang')
-class Lang(Resource):
-    
-    @api.marshal_with(a_lang)
-    def get(self):
-        return langs
-    
-    @api.expect(a_lang)
-    def post(self):
-        new = api.payload
-        new['id'] = len(langs) + 1
-        langs.append(new)
-        return {'result' : 'language added'}, 201
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.register_blueprint(bp, url_prefix="")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- routes/routes.py
+++ routes/routes.py
@@ -1,7 +1,27 @@
-from flask import Blueprint
+from flask import Flask, Blueprint
+from flask_restplus import Api, Resource, fields
 
-second = Blueprint('second', __name__)
+bp = Blueprint('bp', __name__)
+app = Flask(__name__)
+api = Api(app)
+a_lang = api.model('Language', {'language' : fields.String('The language.')})
 
-@second.route('/')
-def home():
-    return "home"
+langs = []
+python = {'language' : 'python', 'id' : 1}
+langs.append(python)
+
+@api.route('/lang')
+class Lang(Resource):
+    
+    @api.marshal_with(a_lang)
+    def get(self):
+        return langs
+    
+    @api.expect(a_lang)
+    def post(self):
+        new = api.payload
+        new['id'] = len(langs) + 1
+        langs.append(new)
+        return {'result' : 'language added'}, 201
+
+from app import *
